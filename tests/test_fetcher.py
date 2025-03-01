d0c7bbcaf902a0c391b79510a23e41257ae5d682
DESKTOP-UQ5TGGD\LENOVO <hafiz300@outlook.com>
2025-03-01 23:36:56 +0000

    added in unit tests for bot and fetcher

--- tests/test_fetcher.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from app.data.fetcher import DexScreenerFetcher
-
-def test_fetcher_initialization():
-    fetcher = DexScreenerFetcher()
-    assert fetcher is not None
-    assert fetcher.dex_base_url == "https://api.dexscreener.com/latest/dex"
